d1a939eb720cc9d4be9d6ab64e01a42e1c802853
Kaizhi Wei <kaizhi@yahoo-inc.com>
2015-05-03 18:44:06 -0700

    landing tweaks

--- client/app/components/hero-landing.js
+++ client/app/components/hero-landing.js
@@ -1,19 +1,47 @@
 import Ember from 'ember';
 
+var navHeight = 65;
+
 export default Ember.Component.extend({
+	actions: {
+		scrollToContent: function () {
+			var top = parseInt(this.get('heroEl').css('height'), 10);
+
+			$('html, body').animate({
+				scrollTop: top - navHeight
+			}, 900);
+		}
+	},
 
 	didInsertElement: function () {
 		this.set('heroEl', $('.hero'));
 
 		$(window).on('resize', Ember.run.bind(this, this.handleResize));
+		$(window).on('scroll', Ember.run.bind(this, this.handleScroll));
+
 		this.get('heroEl').css('height', window.innerHeight);
+		this.set('height', window.innerHeight);
 	},
 
 	handleResize: function (event) {
 		this.get('heroEl').css('height', event.target.innerHeight);
+		this.set('height', event.target.innerHeight);
+	},
+
+	handleScroll: function (event) {
+		var $nav = $('.navbar');
+		console.log(window.scrollY);
+
+		if (window.scrollY > this.get('height') / 2) {
+			$nav.removeClass('top');
+		} else {
+			$nav.addClass('top');
+		}
 	},
 
 	willDestroy: function() {
-		$(window).off('resize');
+		$(window).off();
 	}
+
+
 });
--- client/app/router.js
+++ client/app/router.js
@@ -7,4 +7,6 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
 	this.resource('landing', { path: '/' });
+	this.resource('explore', { path: '/explore' });
+
 });
