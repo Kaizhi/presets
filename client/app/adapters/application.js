5d8441feedc1be587f66338d49262a05e5caa6bb
Kaizhi Wei <kaizhi@yahoo-inc.com>
2015-05-04 00:11:56 -0700

    update hero image

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   coalesceFindRequests: true,
-  namespace: 'api/v1',
+  namespace: 'api',
   //this is dependent on production/development environment
   //It is configured in config/environment.js
   //host: ClientENV.hostUrl
